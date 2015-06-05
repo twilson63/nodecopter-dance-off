3ef152127c1f8a602ca34ba7e2e4e115dee9ac0f
Tom Wilson <tom@jackhq.com>
2015-06-05 12:00:06 -0400

    update example with emergency and land keys

--- example.js
+++ example.js
@@ -3,11 +3,34 @@
 
 var RollingSpider = require("rolling-spider");
 var temporal = require("temporal");
+var keypress = require("keypress");
+
+keypress(process.stdin);
+
+// listen for the 'keypress' event
+
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
 
 var yourDrone = new RollingSpider();
 
 yourDrone.connect(function() {
   yourDrone.setup(function() {
+    process.stdin.on("keypress", function (ch, key) {
+      if (key && key.name === "m") {
+        yourDrone.emergency();
+      }
+      if (key && key.name === "q") {
+        yourDrone.land();
+      }
+      if (key && key.ctrl && key.name === "c") {
+        process.stdin.pause();
+        process.exit(); // eslint-disable-line
+      }
+    });
+
+
     // NEW CODE
     temporal.queue([
       {
@@ -19,13 +42,7 @@ yourDrone.connect(function() {
         }
       },
       {
-        delay: 3000,
-        task: function () {
-          yourDrone.forward();
-        }
-      },
-      {
-        delay: 500,
+        delay: 5000,
         task: function () {
           yourDrone.land();
         }
