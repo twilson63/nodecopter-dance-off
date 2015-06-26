de8bc470eb35db1d1e93ebfd7b939b8586d66ea9
Brent Cobb <brent.tm.cobb@gmail.com>
2015-06-26 13:16:04 -0400

    cleaning things up

--- example.js
+++ example.js
@@ -33,64 +33,12 @@ drone.connect(function() {
 
     // NEW CODE
     temporal.queue([
-      // first step takes care of liftoff, should be ~ 3feet
       {
         delay: 0,
         task: function () {
           drone.flatTrim();
           drone.startPing();
           drone.takeOff();
-        }
-      },
-      {
-        delay: 4000,
-        task: function() {
-          drone.up({steps: 10});
-          drone.backflip():
-        }
-      },
-      {
-        delay: 4000,
-        task: function() {
-          drone.up({steps: 10});
-        }
-      },
-      {
-        delay: 0,
-        task: function() {
-          drone.turnRight({speed: 100});
-          drone.turnRight({speed: 100});
-          drone.turnRight({speed: 100});
-          drone.turnRight({speed: 100});
-        }
-      }
-      // 11ish seconds used so far
-      {
-        delay: 4000,
-        task: function() {
-          drone.leftFlip();
-        }
-      },
-      {
-        delay: 4000,
-        task: function() {
-          drone.rightFlip();
-        }
-      },
-      {
-        delay: 2000,
-        task: function() {
-          drone.down({steps: 20});
-        }
-      },
-      {
-        delay: 0,
-        task: function() {
-          drone.turnLeft({speed: 100});
-          drone.turnLeft({speed: 100});
-          drone.turnLeft({speed: 100});
-          drone.turnLeft({speed: 100});
-          drone.frontFlip({speed: 100});
         }
       },
       {
